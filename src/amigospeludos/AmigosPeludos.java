93eb81b024f466ef03de44b82f84c7f6ad1e33ae
jeasbena <jeasbena@espol.edu.ec>
2021-12-01 00:14:05 -0500

    Se agregaron los ArrayList necesarios en la clase principal

--- src/amigospeludos/AmigosPeludos.java
+++ src/amigospeludos/AmigosPeludos.java
@@ -1,7 +1,19 @@
 
 package amigospeludos;
 
+import entidades.Auspiciante;
+import entidades.Dueño;
+import entidades.Mascota;
+import java.util.ArrayList;
+import lugares.Ciudad;
+
 public class AmigosPeludos {
+    
+    public static final ArrayList<Concurso> concursos = new ArrayList<>();
+    public static final ArrayList<Mascota> mascotas = new ArrayList<>();
+    public static final ArrayList<Ciudad> ciudades = new ArrayList<>();
+    public static final ArrayList<Auspiciante> auspiciantes = new ArrayList<>();
+    public static final ArrayList<Dueño> dueños = new ArrayList<>();
 
     public static void main(String[] args) {
         
